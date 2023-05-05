54fb23389bb80ee419f7375043b59c7946eb9e27
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-05-04 22:31:00 -0700

    Initial stab at demo screen
    TODO:
    - show how to clean up this code by having code to ensure module is
      there and error if it isn't, within the module itself (new step)
    - Fix Hashmap issue on Android (fix up?)
    - Cleaner design on this screen (could fix up)
    - Demonstrate issue with type being mroe specific than it actually
      guarantees
    - Have steps demonstrating promise rejection (use 'get' method without
      starting sensor)
    - Check if codegen library name _actually_ needs to match anything.
      Should wait to check until have iOS impl.

--- NewArchSensorApp/App.tsx
+++ NewArchSensorApp/App.tsx
@@ -5,114 +5,67 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import React, {useCallback, useEffect, useState} from 'react';
+import {Button, SafeAreaView, Text} from 'react-native';
+import RTNOrientation from 'orientation-sensor';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [sensorOn, setSensorOn] = useState(false);
+  const [sensorData, setSensorData] = useState({
+    pitch: undefined,
+    roll: undefined,
+    yaw: undefined,
+  });
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const toggleEvents = useCallback(() => {
+    if (RTNOrientation) {
+      if (sensorOn) {
+        RTNOrientation.stopSensor();
+        setSensorOn(false);
+      } else {
+        RTNOrientation.startSensor();
+        setSensorOn(true);
+      }
+    }
+  }, [sensorOn]);
+
+  const getLastOrientation = useCallback(() => {
+    if (RTNOrientation) {
+      const fn = async () => {
+        // @ts-ignore
+        const last = await RTNOrientation.getLastRecordedOrientation();
+        setSensorData(last);
+      };
+      fn();
+    }
+  }, []);
+
+  // ensure sensor stops
+  // @ts-ignore
+  useEffect(() => {
+    return () => RTNOrientation?.stopSensor();
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Button title={'Toggle events'} onPress={toggleEvents} />
+      <Button title={'Get last orientation'} onPress={getLastOrientation} />
+      <Text>Sensor on:</Text>
+      <Text>{JSON.stringify(sensorOn)}</Text>
+      <Text>Roll</Text>
+      <Text>{sensorData.roll}</Text>
+      <Text>Pitch</Text>
+      <Text>{sensorData.pitch}</Text>
+      <Text>Yaw</Text>
+      <Text>{sensorData.yaw}</Text>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
