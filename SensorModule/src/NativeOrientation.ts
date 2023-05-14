4c17d75c719cdfe670d092ffa3ffa75e18b1b367
Lizzi Lindboe <lindboe@users.noreply.github.com>
2023-05-14 09:40:39 -0700

    Initial JS spec for module
    Codegen demo time

--- SensorModule/src/NativeOrientation.ts
+++ SensorModule/src/NativeOrientation.ts
@@ -2,7 +2,9 @@ import type {TurboModule} from 'react-native';
 import {TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
-  // ...
+  startSensor(): Promise<void>;
+  stopSensor(): Promise<void>;
+  getLastRecordedOrientation(): Promise<{yaw: number, pitch: number, roll: number}>;
 }
 
 export default TurboModuleRegistry.get<Spec>('RTNOrientation');
